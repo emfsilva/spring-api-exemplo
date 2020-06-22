2ec13da51ea7d070a6c59829c9d22d59971355d5
emersonfs <emerson.ferreira@unicid.edu.br>
2020-06-22 12:08:00 -0300

    refatoração service, controller, serviceImpl

--- src/main/java/br/com/exemplospring/exemplo/service/UserService.java
+++ src/main/java/br/com/exemplospring/exemplo/service/UserService.java
@@ -1,14 +1,18 @@
 package br.com.exemplospring.exemplo.service;
 
 import br.com.exemplospring.exemplo.entity.User;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
 
-    Optional<User> findById(long id);
-    User save(User user);
-    void deleteById(Long id) throws Exception;
+    void deleteById(Long id);
     List<User> findAll();
+    boolean exist(Long id);
+    Optional<User> findById(Long id);
+    User regress(Long id);
+    void change(Long id, User user);
+    User save(User user);
 }
